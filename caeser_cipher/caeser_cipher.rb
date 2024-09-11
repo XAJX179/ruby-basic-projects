d802131a2543a1b84610af8596f7b6de2c34e25b
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-09-11 21:39:09 +0530

    Feat: rubocop

--- caeser_cipher/caeser_cipher.rb
+++ caeser_cipher/caeser_cipher.rb
@@ -1,25 +1,24 @@
-def caesar_cipher(string,shift_number)
+def caesar_cipher(string, shift)
   string.codepoints.map do |i|
-    if i >= 65 && i <=90
-      x = i + shift_number
-      if x > 90
-        x-=26
-        x.chr
-      else
-        x.chr
-      end
+    if i >= 65 && i <= 90
+      lower_case(i, shift)
     elsif i >= 97 && i <= 122
-      x = i + shift_number
-      if x > 122
-        x-=26
-        x.chr
-      else
-        x.chr
-      end
+      upper_case(i, shift)
     end
   end.join
 end
 
-  pp caesar_cipher('abcd',2)
-  pp caesar_cipher('abcyz',2)
-  pp caesar_cipher('aBcYZ',2)
+def lower_case(val, shift)
+  x = val + shift
+  x -= 26 if x > 90
+  x.chr
+end
+
+def upper_case(val, shift)
+  x = val + shift
+  x -= 26 if x > 122
+  x.chr
+end
+pp caesar_cipher('abcd', 2)
+pp caesar_cipher('abcyz', 2)
+pp caesar_cipher('aBcYZ', 2)
