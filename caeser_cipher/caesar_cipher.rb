2cc3d2e6b0960f25561f3bae8198a66ae55f8110
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-02-08 11:17:39 +0530

    fix: rename lower and upper case
    they were opposite named

---
+++ caeser_cipher/caesar_cipher.rb
@@ -0,0 +1,24 @@
+def caesar_cipher(string, shift)
+  string.codepoints.map do |i|
+    if i >= 65 && i <= 90
+      upper_case(i, shift)
+    elsif i >= 97 && i <= 122
+      lower_case(i, shift)
+    end
+  end.join
+end
+
+def upper_case(val, shift)
+  x = val + shift
+  x -= 26 if x > 90
+  x.chr
+end
+
+def lower_case(val, shift)
+  x = val + shift
+  x -= 26 if x > 122
+  x.chr
+end
+pp caesar_cipher('abcd', 2)
+pp caesar_cipher('abcyz', 2)
+pp caesar_cipher('aBcYZ', 2)
